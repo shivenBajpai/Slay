80b0eb681e989a709c0f6155481746a6caeaa331
Shiven Bajpai <shiven.bajpai@gmail.com>
2023-05-27 18:46:34 +0530

    Build script fix

--- Slay_Client/build_installer.bat
+++ Slay_Client/build_installer.bat
@@ -1,5 +1,6 @@
 :: Requires WinRAR at the usual installation location, Builds installer exe, run after built.bat
 mkdir ".\installer"
-del ".\installer\SlayInstaller.exe"
+del ".\installer\SlayWinInstaller.exe"
+del ".\installer\Slay.zip"
 "C:\Program Files\WinRAR\WinRAR.exe" a -r -y -m5 -ma5 -afrar -cfg- -k -tl -ep1 -iadm -s -iiconicon.ico -iimgbanner.png -zinstaller_options.txt -sfx ".\installer\SlayWinInstaller.exe" dist\Slay\*
 "C:\Program Files\WinRAR\WinRAR.exe" a -r -y -m5 -ma5 -afzip -cfg- -k -tl -ep1 -s -xdist\Slay\banner.png -xdist\Slay\setup.exe -xdist\Slay\uninstall.exe ".\installer\Slay.zip" dist\Slay\*
--- Slay_Server/build_installer.bat
+++ Slay_Server/build_installer.bat
@@ -1,5 +1,6 @@
 :: Requires WinRAR at the usual installation location, Builds installer exe, run after built.bat
 mkdir ".\installer"
-del ".\installer\SlayServerInstaller.exe"
+del ".\installer\SlayServerWinInstaller.exe"
+del ".\installer\SlayServer.zip"
 "C:\Program Files\WinRAR\WinRAR.exe" a -r -y -m5 -ma5 -afrar -cfg- -k -tl -ep1 -iadm -s -iiconicon.ico -iimgbanner.png -zinstaller_options.txt -sfx ".\installer\SlayServerWinInstaller.exe" "dist\Slay Server\*"
 "C:\Program Files\WinRAR\WinRAR.exe" a -r -y -m5 -ma5 -afzip -cfg- -k -tl -ep1 -s "-xdist\Slay Server\banner.png" "-xdist\Slay Server\setup.exe" "-xdist\Slay Server\uninstall.exe" ".\installer\SlayServer.zip" "dist\Slay Server\*"
