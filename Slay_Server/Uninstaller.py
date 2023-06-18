5fb9968bbdcd498f3c1651b31e2180e1754592e4
Shiven Bajpai <shiven.bajpai@gmail.com>
2023-06-18 15:14:27 +0530

    Server shortcut not being cleared issue

--- Slay_Server/Uninstaller.py
+++ Slay_Server/Uninstaller.py
@@ -4,7 +4,7 @@ import winreg
 
 installPath =  os.environ["ProgramFiles(x86)"] + r'\Slay Server'
 desktopShortcutPath = os.environ["USERPROFILE"] + r'\Desktop\Slay Server.lnk'
-startShortcutPath = os.environ["APPDATA"] + '\\Microsoft\\Windows\\Start Menu\\Programs\\Slay_Server.lnk'
+startShortcutPath = os.environ["APPDATA"] + '\\Microsoft\\Windows\\Start Menu\\Programs\\Slay Server.lnk'
 
 REG_PATH = 'SOFTWARE\Microsoft\Windows\CurrentVersion\\Uninstall'
 KNOWN_FAILS = [installPath+'\\python310.dll',installPath+'\\uninstall.exe',installPath+'\\VCRUNTIME140.dll',installPath+'\\_bz2.pyd',installPath+'\\_lzma.pyd',installPath]
